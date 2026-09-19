e3374cb170c78ecc04db05ab33d4dde471a74de7
huiyilei80-droid <huiyilei80@gmail.com>
2026-09-19 15:38:58 -0700

    Update problems.js

--- lesson1/homework/problems.js
+++ lesson1/homework/problems.js
@@ -1,25 +1,37 @@
 // Problem 1
 // Create a variable for your lucky number and print it.
-
-
-
+const luckyNumber = '???'
+console.log(luckyNumber)
 // Problem 2
-// Create a variable for the number of hours you sleep and print it.
-
-
+// Create a variable for the number of hours you sleep and print i
+const hoursOfSleep = '~9h'
+console.log(hoursOfSleep)
 
 // Problem 3
 // Create a variable for your favorite fruit and print it in a full sentence.
-
-
+const favoriteFruit = 'MANGO!!!!!!!!!!!!!!!!'
+console.log (favoriteFruit)
 
 // Problem 4
 // Create two variables: one for your city and one for your country.
 // Print them on 2 separate lines.
+const city = 'Seattle'
+const country = 'USA'
+console.log (city)
+console.log (country)
 
-
-
+  
 // Problem 5
 // Create 3 variables: your pet's name, its age, and its type.
 // Print them on the same line in a full sentence.
+const petName = 'none'
+const petAge = 'still none'
+const petType = 'NONE!!!!!!!!!!'
+  
+console.log (petName)
+console.log (petAge)
+console.log (petType)
+
+
+
 
