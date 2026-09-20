e6ad166a396c8764bf83350f17907fe1c7182955
huiyilei80-droid <huiyilei80@gmail.com>
2026-09-19 21:30:30 -0700

    Update problems.js

--- lesson1/homework/problems.js
+++ lesson1/homework/problems.js
@@ -9,9 +9,9 @@ console.log(hoursOfSleep)
 
 // Problem 3
 // Create a variable for your favorite fruit and print it in a full sentence.
-const favoriteFruit = 'MANGO!!!!!!!!!!!!!!!!'
+favoriteFruit = 'mango' 
+console.log ("my favorite fruit is"),
 console.log (favoriteFruit)
-
 // Problem 4
 // Create two variables: one for your city and one for your country.
 // Print them on 2 separate lines.
@@ -24,14 +24,28 @@ console.log (country)
 // Problem 5
 // Create 3 variables: your pet's name, its age, and its type.
 // Print them on the same line in a full sentence.
-const petName = 'none'
-const petAge = 'still none'
-const petType = 'NONE!!!!!!!!!!'
+var1 = ("i")
+var2 = ("have")
+var3 = ("no")
+var4 = ("pet")
+var5 = ("so,")
+var6 = ("no")
+var7 = ("pet")
+var8 = ("means")
+var9 = ("no")
+var10 = ("pet")
+var11 = ("name,")
+var12 = ("and")
+var13 = ("that")
+var14 = ("also")
+var15 = ("means")
+var16 = ("no")
+var17 = ("pet")
+var18 = ("type.")
+console.log (var1, var2, var3, var4, var5, var6, var7, var8, var9, var10, var11, var12, var13, var14, var15, var16, var17, var18)
   
-console.log (petName)
-console.log (petAge)
-console.log (petType)
+  
+  
 
 
-
-
+console.log()
