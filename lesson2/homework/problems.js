8e3a5e72370b524796337c1c971a87baaf17783b
huiyilei80-droid <huiyilei80@gmail.com>
2026-09-22 16:51:29 -0700

    Update problems.js

--- lesson2/homework/problems.js
+++ lesson2/homework/problems.js
@@ -1,27 +1,41 @@
 // Problem 1
 // Create two number variables.
 // Print their quotient and remainder on separate lines.
-
-
+const num1 = "15";
+const num2 = "7";
+const quotient = "2";
+const remainder = "1";
+console.log (num1 + "/" + num2)
+console.log (quotient)
+console.log (remainder)
 
 // Problem 2
 // Create variables for your favorite animal and favorite color.
 // Print a sentence combining them like: "A blue tiger would be awesome!"
-
-
+const favoriteColor = "teal ";
+const favoriteAnimal = "orca ";
+console.log ("a " + favoriteColor + favoriteAnimal + "would be my dream pet!")
 
 // Problem 3
 // Use a for loop to print all the even numbers from 0 to 10 (including 10).
+let count = 0;
+
+while (count < 12) {
+  console.log(count);
+  count += 2; 
+}
 
 
 
 // Problem 4
 // Create a variable for how many push-ups you can do.
 // Multiply it by 7 and print how many you could do in a week.
-
-
+pushUpCount = 4
+console.log (pushUpCount + "*" + "7" + "=" + "28")
 
 // Problem 5
 // Use a for loop to print the square of each number from 1 to 6.
 // (Example: 1*1=1, 2*2=4, etc.)
-
+for (let x = 1; x <= 6; x++) {
+    console.log(x * x);
+}
